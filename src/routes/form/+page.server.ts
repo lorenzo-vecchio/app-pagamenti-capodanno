538472a0ac6de409d483b43333b886c87d545420
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2024-12-10 19:30:23 +0100

    validazione base form

--- src/routes/form/+page.server.ts
+++ src/routes/form/+page.server.ts
@@ -1,10 +1,25 @@
-import type { ServerLoad } from "@sveltejs/kit";
+import type { ServerLoad, Actions } from "@sveltejs/kit";
+import { fail } from "@sveltejs/kit";
 import { superValidate } from "sveltekit-superforms";
 import { formSchema } from "./settings/schema";
 import { zod } from "sveltekit-superforms/adapters";
 
 export const load: ServerLoad = async () => {
-  return {
-    form: await superValidate(zod(formSchema)),
-  };
+    return {
+        form: await superValidate(zod(formSchema)),
+    };
+};
+
+export const actions: Actions = {
+    default: async (event) => {
+        const form = await superValidate(event, zod(formSchema));
+        if (!form.valid) {
+            return fail(400, {
+                form,
+            });
+        }
+        return {
+            form,
+        };
+    },
 };
