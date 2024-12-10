6ba39e8957eb7c18c8cb5a523bc56640145bbde1
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2024-12-10 21:59:49 +0100

    format update

--- src/routes/form/settings/schema.ts
+++ src/routes/form/settings/schema.ts
@@ -1,11 +1,21 @@
+import { message } from "sveltekit-superforms";
 import { date, z } from "zod";
 
 export const formSchema = z.object({
-  name: z.string().min(2).max(50),
-  lastName: z.string().min(2).max(50),
-  instagramUsername: z.string().min(2).max(50),
-  email: z.string().email(),
-  dateOfBirth: z.string().date(),
+  name: z
+    .string()
+    .min(2, { message: "Name must be at least 2 characters" })
+    .max(50, { message: "Name must be at most 50 characters" }),
+  lastName: z
+    .string()
+    .min(2, { message: "Last Name must be at least 2 characters" })
+    .max(50, { message: "Last Name must be at most 50 characters" }),
+  instagramUsername: z
+    .string()
+    .min(2, { message: "Username must be at least 2 characters" })
+    .max(50, { message: "Username must be at most 50 characters" }),
+  email: z.string().email({ message: "Invalid email" }),
+  dateOfBirth: z.string().date("Invalid date"),
 });
 
 export type FormSchema = typeof formSchema;
