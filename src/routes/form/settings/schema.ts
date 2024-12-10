6e0a0cfcc3d0862a0193818e44c4c1158ed4c62f
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2024-12-10 19:47:05 +0100

    update validation

--- src/routes/form/settings/schema.ts
+++ src/routes/form/settings/schema.ts
@@ -4,6 +4,7 @@ export const formSchema = z.object({
   name: z.string().min(2).max(50),
   lastName: z.string().min(2).max(50),
   instagramUsername: z.string().min(2).max(50),
+  email: z.string().email(),
   dateOfBirth: date(),
 });
 
