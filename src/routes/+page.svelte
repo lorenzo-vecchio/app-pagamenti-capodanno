4e2b00236a5de8e0133dbc817cb8e710ca641b8d
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2024-12-13 15:20:26 +0100

    Enhance event description with bold formatting for key concepts and improved readability

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
   <Lottie animationData={projectx} className="max-w-sm glow" />
   <div class="flex flex-col justify-center items-center p-5 mx-5 border rounded-xl border-2 max-w-4xl">
     <p class="text-xl text-center text-gray-500 bg-clip-text bg-gradient-to-r from-customStart to-customEnd mb-3">
-      Un nuovo <span class="text-transparent">concept</span> di eventi che unisce l’energia di una <span class="text-transparent">discoteca</span> con il divertimento informale di una <span class="text-transparent">festa in casa</span>. Appartamenti, ville, loft: la location cambia, ma il divertimento resta sempre la costante.
+      Un <span class="text-transparent font-bold">nuovo concept</span> di eventi che unisce il divertimento di una <span class="text-transparent font-bold">discoteca</span> con le vibes di una <span class="text-transparent font-bold">festa in casa</span>. <span class="font-bold text-gray-300">Appartamenti, ville, loft:</span> la location cambia, ma il <span class="font-bold text-gray-300">divertimento</span> resta sempre la costante.
     </p>
     <GradButton
       class="mt-4 text-2xl p-6 bg-gradient-to-r from-customStart to-customEnd w-fit"
