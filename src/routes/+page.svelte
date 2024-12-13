a4236e7097d9033b1635669b4103583bcc6eea8b
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2024-12-13 15:13:29 +0100

    Update event description for clarity and conciseness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
   <Lottie animationData={projectx} className="max-w-sm glow" />
   <div class="flex flex-col justify-center items-center p-5 mx-5 border rounded-xl border-2 max-w-4xl">
     <p class="text-xl text-center text-gray-500 bg-clip-text bg-gradient-to-r from-customStart to-customEnd mb-3">
-      Un nuovo <span class="text-transparent">concept</span> di eventi che unisce l’energia di una <span class="text-transparent">discoteca</span> con il divertimento informale di una <span class="text-transparent">festa in casa</span>. <span class="text-gray-400 font-bold">Dove?</span> Appartamenti, ville, loft o spazi eventi: la location cambia ogni volta, ma l’unica costante è il divertimento senza compromessi.
+      Un nuovo <span class="text-transparent">concept</span> di eventi che unisce l’energia di una <span class="text-transparent">discoteca</span> con il divertimento informale di una <span class="text-transparent">festa in casa</span>. Appartamenti, ville, loft: la location cambia, ma il divertimento resta sempre la costante.
     </p>
     <GradButton
       class="mt-4 text-2xl p-6 bg-gradient-to-r from-customStart to-customEnd w-fit"
